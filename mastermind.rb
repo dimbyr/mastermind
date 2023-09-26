65ec38c510e34ee60700ca5dad8efb49d27bbc72
Dimby Rabearivony <drabearivony@gmail.com>
2023-09-26 23:38:33 +0200

    The CodeMaker class is already working Not yet complete though

--- mastermind.rb
+++ mastermind.rb
@@ -1,16 +1,32 @@
-color_list = %w[Red Blue Green Yellow Purple Orange]
-
 # a selector
-class Selector
-  attr_accessor :hidden_colors
+class CodeMaker
+  attr_accessor :secret_colors
 
   def initialize
-    @hidden_colors = []
+    color_list = %w[Red Blue Green Yellow Purple Orange]
+    @secret_colors = []
+    4.times do
+      @secret_colors << color_list.sample
+    end
   end
 
   def score_a_guess(guess)
-    # check for matches: How many right colors are there?
-    # Among the right colors, how many of them are in the right place?
+    score_table = {
+      right_color_right_position: 0, 
+      right_color_wrong_position: 0,
+      wrong_color_wrong_position: 0
+      }
+    
+    guess.each_with_index do |x, i|
+      if @secret_colors[i] == x 
+        score_table[:right_color_right_position] += 1
+      elsif @secret_colors.include?(x)
+        score_table[:right_color_wrong_position] += 1
+      else 
+        score_table[:wrong_color_wrong_position] += 1
+      end
+    end
+    score_table
   end
 
   def select_random_colors
@@ -20,3 +36,13 @@ class Selector
   end
 
 end
+
+class CodeBroker
+end
+
+mastermind = CodeMaker.new()
+guess = %w[Red Blue Green Green]
+puts "Your scores are the following:\n #{mastermind.score_a_guess(guess)}"
+
+
+
