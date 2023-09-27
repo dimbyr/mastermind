deb7f88bcb2feb178dd71e34ba31ee753fad94b9
Dimby Rabearivony <drabearivony@gmail.com>
2023-09-27 13:19:14 +0200

    Lines 20 -- 27 still need to be fixed

--- mastermind.rb
+++ mastermind.rb
@@ -1,12 +1,12 @@
 # a selector
 class CodeMaker
   attr_accessor :secret_colors
+  $color_list = %w[Red Blue Green Yellow Purple Orange]
 
   def initialize
-    color_list = %w[Red Blue Green Yellow Purple Orange]
     @secret_colors = []
     4.times do
-      @secret_colors << color_list.sample
+      @secret_colors << $color_list.sample
     end
   end
 
@@ -14,7 +14,7 @@ class CodeMaker
     score_table = {
       right_color_right_position: 0, 
       right_color_wrong_position: 0,
-      wrong_color_wrong_position: 0
+      wrong_colors: 0
       }
     
     guess.each_with_index do |x, i|
@@ -23,7 +23,7 @@ class CodeMaker
       elsif @secret_colors.include?(x)
         score_table[:right_color_wrong_position] += 1
       else 
-        score_table[:wrong_color_wrong_position] += 1
+        score_table[:wrong_colors] += 1
       end
     end
     score_table
@@ -31,18 +31,63 @@ class CodeMaker
 
   def select_random_colors
     4.times do
-      @hidden_colors << color_list.sample(4)
+      @hidden_colors << $color_list.sample(4)
     end
   end
 
 end
 
 class CodeBroker
+  attr_accessor :guess
+  def initialize
+    @guess = []
+    4.times do 
+      @guess << $color_list.sample
+    end
+  end
+
+  def update_guess(feedback)
+    @gess.map! {|color| $color_list.sample}
+  end
+
+end
+
+class HumanGuesser < CodeBroker
+
+  def update_guess()
+    puts "Select 4 colors using numbers from 1-6 (NO SPACE). Duplicates are allowed. eg. 1111 is [Red Red Red Red]"
+    $color_list.each_with_index do |color, index|
+      print "#{index + 1}:  #{color},\t"
+    end
+    puts "\n"
+    choice = gets.chomp.split('').map! {|x| x.to_i}
+    until (choice.length == 4 && choice.all? {|x| x >= 1 && x <= 6}) do
+      puts "Please enter a sequence of FOUR numbers (no space) between 1-6 to choose 4 colors"
+      $color_list.each_with_index do |color, index|
+        print "#{index + 1}:  #{color},\t"
+      end
+      puts "\n"
+      choice = gets.chomp.split('').map! {|x| x.to_i}
+    end
+    @guess = choice.map {|x| $color_list[x-1]}
+  end
 end
 
 mastermind = CodeMaker.new()
-guess = %w[Red Blue Green Green]
-puts "Your scores are the following:\n #{mastermind.score_a_guess(guess)}"
-
-
-
+Human = HumanGuesser.new()
+track = 0
+n = 12
+n.times do
+  guess = Human.update_guess
+  scores = mastermind.score_a_guess(guess)
+  puts "Your scores are the following:\n #{scores}"
+  if scores[:right_color_right_position] == 4
+    puts "Congratulations, You got it! The secret colors are\n #{mastermind.secret_colors}"
+    break
+  end
+  track += 1
+  puts "#{n-track} attempts remaining !!! "
+end
+if track == n 
+  puts "Game Over! You could not guess the secret colors\n They are : #{mastermind.secret_colors}"
+end
